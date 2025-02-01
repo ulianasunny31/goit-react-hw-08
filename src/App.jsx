117eaeb79cd81f4cf63f18a4c6ba761245418a6d
Uliana Seredenko <uliana1331@gmail.com>
2025-02-01 11:15:57 +0100

    restricted+private route

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import { lazy } from 'react';
 // import { fetchContacts } from './redux/contacts/operations';
 // import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
-
 import Layout from './components/Layout';
+import PrivateRoute from './components/PrivateRoute';
+import RestrictedRoute from './components/RestrictedRoute';
 
+//imports
 const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
 const RegisterPage = lazy(() =>
   import('./pages/RegistrationPage/RegistrationPage')
@@ -13,6 +15,7 @@ const RegisterPage = lazy(() =>
 const LoginPage = lazy(() => import('./pages/LoginPage/LoginPage'));
 const ContactsPage = lazy(() => import('./pages/ContactsPage/ContactsPage'));
 
+//app component
 function App() {
   // const dispatch = useDispatch();
 
@@ -26,9 +29,27 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/contacts" element={<ContactsPage />} />
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute
+                redirectTo="/tasks"
+                component={<RegisterPage />}
+              />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute redirectTo="/tasks" component={<LoginPage />} />
+            }
+          />
+          <Route
+            path="/contacts"
+            element={
+              <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+            }
+          />
         </Routes>
       </Layout>
     </>
